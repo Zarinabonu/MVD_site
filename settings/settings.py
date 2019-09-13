4f20f0038567e884a509630c52450ac734e04fda
unknown <a@a.com>
2019-09-13 18:00:10 +0500

    aa

--- settings/settings.py
+++ settings/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nb22)3(z+8n9$33ncp1bt*up6neaizqky)8-o&2hb9kjqdc6*$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -85,10 +85,8 @@ WSGI_APPLICATION = 'wsgi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mvdsite',
-        'USER': 'mvduser',
-        'PASSWORD': '12345',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
